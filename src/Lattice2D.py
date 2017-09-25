8631dfdfb4fdf650f6b4d136f262f3e60fd68839
Dilyn Fullerton <dilyn.fullerton@att.net>
2017-09-25 17:55:52 -0400

    Fixed bugs. Lattice2D seems to work

--- src/Lattice2D.py
+++ src/Lattice2D.py
@@ -2,6 +2,7 @@ from itertools import product, count, chain
 from numpy import exp, dot, array, sqrt
 from scipy.linalg import sqrtm, eigh, inv
 import numpy as np
+from qutip import *
 
 
 def _bloch_equiv(q1, q2):
@@ -82,7 +83,6 @@ class UnitCell2D:
         """
         self.a1 = array(a1)
         self.a2 = array(a2)
-        self._a_mat = np.hstack(self.a1, self.a2)
         self.particle_positions = particle_positions
         self.particle_masses = particle_masses
         self.internal_connections = _all_connections(internal_connections)
@@ -134,10 +134,10 @@ class BlochVector:
         if not isinstance(other, BlochVector):
             return False
         else:
-            return self.canonical_form() == other.canonical_form()
+            return self.canonical_form().all() == other.canonical_form().all()
 
     def __hash__(self):
-        return hash(self.canonical_form())
+        return int(dot(self.q, self.q))
 
     def __neg__(self):
         return BlochVector(-self.q)
@@ -154,23 +154,30 @@ class BlochVector:
     def __getitem__(self, item):
         return self.q[item]
 
+    def __len__(self):
+        return len(self.q)
+
     def canonical_form(self):
         return array([qi - int(qi) for qi in self.q])
 
+    def dot(self, other):
+        assert len(other) == len(self)
+        other = array(other)
+        return np.dot(self.q, other)
+
 
 class Lattice2D:
-    def __init__(self, unit_cell, N_x, N_y, c_matrix, x_ops, p_ops):
+    def __init__(self, unit_cell, N_x, N_y, c_matrix):
         self.unit_cell = unit_cell
         self.Nx = N_x
         self.Ny = N_y
+        self.M = self.unit_cell.num_particles
         # self.b1, self.b2 = self._set_b_vectors()
         self.c_matrix = c_matrix
-        self._x_ops = x_ops
-        self._p_ops = p_ops
         self._evals_dict = dict()
         self._evect_dict = dict()
         self._indices = list(
-            product(range(self.unit_cell.num_particles), range(2))
+            product(range(self.M), range(2))
         )
 
     def are_connected(self, k1, p1, k2, p2):
@@ -185,13 +192,13 @@ class Lattice2D:
         nx2, ny2 = p2
         if abs(nx1-nx2) + abs(ny1-ny2) > 1:
             return False
-        elif nx1 - nx2 == 1:
+        elif nx1 - nx2 == 1 or nx2 - nx1 == self.Nx - 1:
             return self.unit_cell.connected_x(k2, k1)
-        elif nx2 - nx1 == 1:
+        elif nx2 - nx1 == 1 or nx1 - nx2 == self.Nx - 1:
             return self.unit_cell.connected_x(k1, k2)
-        elif ny1 - ny2 == 1:
+        elif ny1 - ny2 == 1 or ny2 - ny1 == self.Ny - 1:
             return self.unit_cell.connected_y(k2, k1)
-        elif ny2 - ny1 == 1:
+        elif ny2 - ny1 == 1 or ny1 - ny2 == self.Ny - 1:
             return self.unit_cell.connected_y(k1, k2)
         else:
             return self.unit_cell.connected_int(k1, k2)
@@ -199,16 +206,18 @@ class Lattice2D:
     def position(self, k, p):
         return self.unit_cell.position(k, p)
 
+    def displacement(self, k1, p1, k2, p2):
+        return self.position(k2, p2) - self.position(k1, p1)
+
     def unit_cells(self):
         return product(range(self.Nx), range(self.Ny))
 
     def d_matrix(self, k1, x1, k2, x2):
         def _d(q):
             d = 0
-            for nx, ny in self.unit_cells():
-                dp = self.c_matrix(self, k1, x1, (0, 0), k2, x2, (nx, ny))
-                q = array(q)
-                dp *= exp(1j * 2*np.pi * (q[0] * nx + q[1] * ny))
+            for p in self.unit_cells():
+                dp = self.c_matrix(self, k1, x1, p, k2, x2, p)
+                dp *= exp(1j * 2*np.pi * q.dot(p))
                 d += dp
             return d * 1/sqrt(self.unit_cell.mass(k1) * self.unit_cell.mass(k2))
         return _d
@@ -219,7 +228,7 @@ class Lattice2D:
         def _e(q):
             if q not in self._evals_dict:
                 self._set_d_eigenvectors(q)
-            return self._evect_dict[q][:, v][i]
+            return self._evect_dict[q][v][i]
         return _e
 
     def omega2(self, q, v):
@@ -228,20 +237,20 @@ class Lattice2D:
         return self._evals_dict[q][v]
 
     def q_vectors(self):
-        for m1, m2 in product(range(self.Nx), range(self.Ny)):
+        for m1, m2 in self.unit_cells():
             yield BlochVector([m1/self.Nx, m2/self.Ny])
 
     def _A(self):
         for q in self.q_vectors():
             if q == -q:
-                yield q.canonical_form()
+                yield q
 
     def _B(self):
         b = []
         for q in self.q_vectors():
             if q != -q and -q not in b:
                 b.append(q)
-                yield q.canonical_form()
+                yield q
 
     def operator_q_vectors(self):
         return chain(self._A(), self._B())
@@ -255,13 +264,27 @@ class Lattice2D:
             y, py = self._y(q, v)
             return 1/2 * (x + 1j*px) + 1j/2 * (y + 1j*py)
 
+    def _ops(self, k, x, p, op):
+        dim = 2 * self.M * self.Nx * self.Ny
+        nx, ny = p
+        xop = [qeye(2)] * dim
+        idx = x + 2*k + 2*self.M*nx + 2*self.M*self.Nx*ny
+        xop.insert(idx, op)
+        xop.pop(idx+1)
+        return tensor(xop)
+
+    def _x_ops(self, k, x, p):
+        return self._ops(k, x, p, op=position(2))
+
+    def _p_ops(self, k, x, p):
+        return self._ops(k, x, p, op=momentum(2))
+
     def _z(self, q, v):
         real_z = 0+0j
         imag_z = 0+0j
         real_pz = 0+0j
         imag_pz = 0+0j
-        for k, x, p in product(range(self.unit_cell.num_particles), range(2),
-                               product(range(self.Nx), range(self.Ny))):
+        for k, x, p in product(range(self.M), range(2), self.unit_cells()):
             zi = exp(-1j * 2*np.pi * (q[0]*p[0] + q[1]*p[1]))
             zi *= sqrt(self.unit_cell.mass(k)/self.unit_cell.mass(0))
             zi *= np.conj(self.e(k, x, v)(q)) / sqrt(self.Nx * self.Ny)
@@ -270,13 +293,13 @@ class Lattice2D:
             real_z += zi.real * xop
             imag_z += zi.imag * xop
             real_pz += zi.real * pop
-            imag_pz += zi.imega * pop
+            imag_pz += zi.imag * pop
         return real_z, imag_z, real_pz, imag_pz
 
     def _l(self, q, v):
         m0 = self.unit_cell.mass(0)
-        omega = sqrt(self.omega2(q, v))
-        return 1 / sqrt(2 * m0 * omega)
+        omega2 = self.omega2(q, v)+0j
+        return 1 / sqrt(2 * m0 * sqrt(omega2))
 
     def _x(self, q, v):
         real_z, imag_z, real_pz, imag_pz = self._z(q, v)
@@ -301,7 +324,7 @@ class Lattice2D:
     def _get_matrix_rep_d(self, q):
         nparts = self.unit_cell.num_particles
         dim = nparts * 2
-        d_mat = np.empty(shape=(dim, dim))
+        d_mat = np.empty(shape=(dim, dim), dtype=np.complex)
         for k1x1, i in zip(product(range(nparts), range(2)), count()):
             k1, x1 = k1x1
             for k2x2, j in zip(product(range(k1, nparts), range(x1, 2)),
@@ -314,10 +337,8 @@ class Lattice2D:
 
     def _orthonormal_eigenvectors(self, dmat):
         evals, evects = eigh(a=dmat)
-        on_evects = dot(evects, inv(sqrtm(dot(evects.H, evects))))
-        for eval, evect in zip(evals, on_evects):
-            assert dmat * evect == eval * evect
-        return evals, on_evects
+        on_evects = dot(evects, inv(sqrtm(dot(evects.conj().T, evects))))
+        return evals, [on_evects[:, i] for i in range(len(evals))]
 
     def _set_d_eigenvectors(self, q):
         evals, evects = self._orthonormal_eigenvectors(
@@ -326,14 +347,33 @@ class Lattice2D:
         self._evect_dict[q] = evects
 
 
-def cmat_coulomb2d(lattice2d, k1, x1, p1, k2, x2, p2):
-    if not lattice2d.are_connected(k1, p1, k2, p2):
+def get_cmat_harmonic_interaction2d(k, g):
+    def cmat_harmonic_interaction2d(lattice2d, k1, x1, p1, k2, x2, p2):
+        if (k1, x1, p1) == (k2, x2, p2):
+            return k
+        elif lattice2d.are_connected(k1, p1, k2, p2):
+            return -g/2
         return 0
-    else:
-        disp = lattice2d.position(k1, p1) - lattice2d.position(k2, p2)
-        disp_abs = abs(disp)
-        offdiag = 3 * disp[x1] * disp[x2] / disp_abs**5
-        if x1 != x2:
-            return offdiag
-        else:
-            return offdiag - 1 / disp_abs**3
+    return cmat_harmonic_interaction2d
+
+
+# Simple example
+line_x = UnitCell2D(
+    a1=[1, 0], a2=[0, 1],
+    particle_positions=[0],
+    particle_masses=[1],
+    internal_connections=[],
+    external_connections_x=[(0, 0)],
+    external_connections_y=[(0, 0)]
+)
+
+c_mat = get_cmat_harmonic_interaction2d(k=1, g=1/100)
+
+lattice = Lattice2D(
+    unit_cell=line_x, N_x=2, N_y=2, c_matrix=c_mat,
+)
+
+for q in lattice.operator_q_vectors():
+    for v in range(lattice.M * 2):
+        a = lattice.annihilation_operator(q, v)
+        print(a)
