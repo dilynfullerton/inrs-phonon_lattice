c87bcd90d89a58017ebe50183b5b93eb447f8be3
Dilyn Fullerton <dilyn.fullerton@att.net>
2017-09-26 10:08:27 -0400

    Removed GaAs. Trying to get 1d harmonic oscillator line to work first

--- tests/examples.py
+++ tests/examples.py
@@ -18,7 +18,7 @@ def get_c_matrix_simple_harmonic_interaction(k):
         if (k1, x1, p1.all()) == (k2, x2, p2.all()):
             return k * lattice.num_connections(k1, p1)
         elif x1 == x2 and lattice.are_connected(k1, p1, k2, p2):
-            return -1/2 * k
+            return -k
         else:
             return 0
     return c_matrix
@@ -50,60 +50,39 @@ def get_c_matrix_coulomb_interaction(g):
 
 
 # Construct unit cell, a simple line with two masses
-MASS_Ga = 69.723  # [amu]
-MASS_As = 74.922  # [amu]
-A = 1  # Unit cell width
-G = 1  # e^2 / (4 pi epsilon0)
-
-GaAs = UnitCell3D(
-    a1=[A, 0, 0], a2=[0, A, 0], a3=[0, 0, A],
-    particle_positions=[
-        # corners
-        [0, 0, 0],
-        # faces
-        [1/2, 1/2, 0], [1/2, 0, 1/2], [0, 1/2, 1/2],
-        # center
-        [1/4, 1/4, 1/4], [3/4, 3/4, 1/4], [3/4, 1/4, 3/4], [1/4, 3/4, 3/4]
-    ],
-    particle_masses=[MASS_Ga]*4+[MASS_As]*4,
-    internal_connections=[
-        (0, 4), (1, 4), (2, 4), (3, 4),
-        (1, 5), (2, 6), (3, 7)
-    ],
-    external_connections_x=[(5, 3), (6, 3)],
-    external_connections_y=[(7, 2), (5, 2)],
-    external_connections_z=[(6, 1), (7, 1)],
-    external_connections_xy=[(5, 0)],
-    external_connections_xz=[(6, 0)],
-    external_connections_yz=[(7, 0)],
+line1d = UnitCell1D(
+    a1=[1],
+    particle_positions=[0, 1/2],
+    particle_masses=[.1, 1.],
+    internal_connections=[(0, 1)],
+    external_connections_x=[(1, 0)]
 )
 
+# Verify number of connections
+print('num_connections(k={})= {}'.format(0, line1d.num_connections(0)))
+print('num_connections(k={})= {}'.format(1, line1d.num_connections(1)))
+
 # Construct lattice
-lat = PhononLattice3D(
-    unit_cell=GaAs, N_x=NX, N_y=NY, N_z=NZ,
-    c_matrix=get_c_matrix_coulomb_interaction(g=G)
+lat = PhononLattice1D(
+    unit_cell=line1d, N_x=NX,
+    c_matrix=get_c_matrix_simple_harmonic_interaction(k=K)
 )
 
 # Plot eigenvalues vs. q
-xdats = [[] for i in range(3)]
-ydats = [[] for i in range(lat.dim_d)]
+xdat = []
+ydats = [[] for v in range(lat.dim_d)]
 labs = [
     'omega_{q, ' + '{}'.format(v) + '}^2' for v in range(lat.dim_d)]
-for q in lat._A():
-    for i in range(3):
-        xdats[i].append(q[i])
+for q in lat.q_vectors():
+    xdat.append(q[0])
     for v in range(lat.dim_d):
         ydats[v].append(lat.omega2(q, v))
 
-for v in range(lat.dim_d):
-    ydats[v] = [q - ydats[v][0] for q in ydats[v]]
+plots = sorted([(xdat, ydat, lab) for ydat, lab in zip(ydats, labs)])
 
-plots = sorted([[(x, y, lab) for y, lab in zip(ydats, labs)] for x in xdats])
-
-for i in range(3):
-    fig, ax = plt.subplots(1, 1)
-    for xdat, ydat, lab in plots[i]:
-        ax.plot(xdat, ydat, '-', label=lab)
-    ax.set_xlabel('Bloch wavevector, q[{}]'.format(i))
-    ax.set_ylabel('Eigenfrequency, omega_{q,v}^2')
-    plt.show()
+fig, ax = plt.subplots(1, 1)
+for xdat, ydat, lab in plots:
+    ax.plot(xdat, ydat, '-', label=lab)
+ax.set_xlabel('Bloch wavevector, q[0]')
+ax.set_ylabel('Eigenfrequency, omega_{q,v}^2')
+plt.show()
