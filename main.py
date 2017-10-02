c08e498573a1717a98af02f07236c17b0eb1b5a1
Dilyn Fullerton <dilyn.fullerton@att.net>
2017-10-02 13:42:33 -0400

    Attempt to do CdS

--- main.py
+++ main.py
@@ -3,40 +3,45 @@ from PhononLattice import *
 from UnitCell import *
 from PhononLattice import get_c_matrix_coulomb_interaction
 
-UNIT_LENGTH_M = 1
-UNIT_MASS_KG = 1
-UNIT_CURRENT_A = 1
-UNIT_TIME_S = 1
+# UNIT_MASS_KG = 1
+# UNIT_LENGTH_M = 1
+# UNIT_CHARGE_C = 1
+# UNIT_TIME_S = 1
+#
+# UNIT_LENGTH_CM = UNIT_LENGTH_M / 100
+# UNIT_MASS_G = 1/1000 * UNIT_MASS_KG
+# UNIT_MASS_AMU = 1.6605e-27 * UNIT_MASS_KG
+# UNIT_CHARGE_E = 1.602e-19 * UNIT_CHARGE_C
+# UNIT_CAPACITANCE_F = UNIT_TIME_S**2 * UNIT_CHARGE_C**2 / UNIT_LENGTH_M**2 / UNIT_MASS_KG
 
-UNIT_LENGTH_CM = UNIT_LENGTH_M / 100
-UNIT_MASS_G = 1/1000 * UNIT_MASS_KG
-UNIT_MASS_AMU = 1.6605e-27 * UNIT_MASS_KG
-UNIT_CHARGE_C = UNIT_TIME_S * UNIT_CURRENT_A
-UNIT_CHARGE_E = 1.602e-19 * UNIT_CHARGE_C
-UNIT_CAPACITANCE_F = UNIT_TIME_S**4 * UNIT_CURRENT_A**2 / UNIT_LENGTH_M**2 / UNIT_MASS_KG
+UNIT_MASS_AMU = 1/100
+UNIT_CHARGE_E = 1
+UNIT_DENSITY_G_CM3 = 1
+UNIT_PERMITIVITY_EPSILON_0 = 1e12
 
 MASS_Cd = 112.414 * UNIT_MASS_AMU
 MASS_S = 32.06 * UNIT_MASS_AMU
-DENSITY_CdS = 4.826 * UNIT_MASS_G / UNIT_LENGTH_CM**3
+DENSITY_CdS = 4.826 * UNIT_DENSITY_G_CM3
 uc_CdS = zincblende3d(
     a=DENSITY_CdS,
     a_is_density=True,
     mass1=MASS_Cd,
+    charge1=+2*UNIT_CHARGE_E,
     mass2=MASS_S,
+    charge2=-2*UNIT_CHARGE_E,
     phi=0, psi=0, theta=0,
-    particle_charges=[+2 * UNIT_CHARGE_E, -2 * UNIT_CHARGE_E]
 )
 
 NX = 40  # Number of particles along line
 NY = 2
 NZ = 2
 
-EPSILON_0 = 8.854e-12 * UNIT_CAPACITANCE_F / UNIT_LENGTH_M
+EPSILON_0 = 8.854e-12 * UNIT_PERMITIVITY_EPSILON_0
 K_COULOMB = 1 / 4 / np.pi / EPSILON_0
 lat_CdS = PhononLattice3D(
     unit_cell=uc_CdS,
     N_x=NX, N_y=NY, N_z=NZ,
-    c_matrix=get_c_matrix_coulomb_interaction2(K_COULOMB)
+    c_matrix=get_c_matrix_coulomb_interaction(K_COULOMB)
 )
 
 
@@ -47,14 +52,17 @@ ydats = [[] for v in range(lat.num_modes)]
 labs = [
     '$\omega_{q, ' + '{}'.format(v) + '}^2$' for v in range(lat.num_modes)]
 for q in sorted(lat.operator_q_vectors()):
-    if q[0] == 0:
+    qdisp = q.canonical_position()
+    if qdisp[0] == 0:
         continue
-    if q[1] != 0 or q[2] != 0:
+    if qdisp[1] != 0 or qdisp[2] != 0:
         continue
-    print(q)
-    xdat.append(q[0])
+    print('qx={:4.2}'.format(float(qdisp[0])))
+    xdat.append(qdisp[0])
     for v in range(lat.num_modes):
-        ydats[v].append(lat.omega2(q, v))
+        omega2 = lat.omega2(q, v)
+        print('  omega2={:8.4}'.format(omega2))
+        ydats[v].append(omega2)
 
 plots0 = [(xdat, ydat, lab) for ydat, lab in zip(ydats, labs)]
 
